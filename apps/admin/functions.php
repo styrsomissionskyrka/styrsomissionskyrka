cf2c0b0b69d0c3d18c7afdff7f0aea63340bce31
Adam Bergman <adam.bergman@klarna.com>
2022-07-07 09:31:56 +0200

    Setup wp-bundler

--- apps/admin/functions.php
+++ apps/admin/functions.php
@@ -4,16 +4,8 @@ namespace StyrsoMissionskyrka;
 
 require_once __DIR__ . '/inc/polyfills.php';
 require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/inc/AssetLoader.php';
 
-\add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\enqueue_scripts');
+AssetLoader::prepare();
 
-function enqueue_scripts()
-{
-  \wp_enqueue_script(
-    'styrso-missionskyrka',
-    \get_template_directory_uri() . '/dist/admin.global.js',
-    [],
-    '1.0.0',
-    true
-  );
-}
+AssetLoader::enqueueAssets('admin');
---
+++ apps/admin/scripts/package.mjs
@@ -0,0 +1,34 @@
+import { accessSync, rmSync } from 'node:fs';
+import { dirname, join } from 'node:path';
+
+import AdmZip from 'adm-zip';
+import { globbySync } from 'globby';
+
+(() => {
+  let cwd = process.cwd();
+  let zipPath = join(cwd, '../../dist/apps/admin/styrsomissionskyrka.zip');
+  let items = globbySync([
+    '*.php',
+    'style.css',
+    'screenshot.png',
+    'README.md',
+    '(assets|dist|inc|languages|vendor|views)/**/*',
+  ]);
+
+  let z = new AdmZip();
+  for (let item of items) {
+    z.addLocalFile(item, dirname(item));
+  }
+
+  if (exists(zipPath)) rmSync(zipPath);
+  z.writeZip(zipPath);
+})();
+
+function exists(file) {
+  try {
+    accessSync(file);
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
